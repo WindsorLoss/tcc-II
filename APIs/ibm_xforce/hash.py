afcdc5e1c2d34e5f3790a8ad7b1e45de3a4a0c60
Windsor Lóss <windsorlossassismonteiro@gmail.com>
2023-11-05 09:58:53 -0300

    Mensagem de erro caso o status code seja 404

--- APIs/ibm_xforce/hash.py
+++ APIs/ibm_xforce/hash.py
@@ -8,55 +8,59 @@ def xfr_get_hash(api, hash):
     response = requests.get(f"https://api.xforce.ibmcloud.com/api/malware/{hash}", headers= {
         "Authorization": f"Basic {api}",
         "accept": "application/json"
-    }).json()
+    })
 
     try:
 
-        malware = response['malware']
-        origins = malware['origins']
-        risco = malware['risk'].upper()
-        risco = risco == 'LOW' and Fore.GREEN + f"{risco}" or (risco == 'MEDIUM' and Fore.YELLOW + f"{risco}" or Fore.RED + f"{risco}")
-        tags = response['tags']
+        if response.status_code == 404:
+            print(Fore.RED + Style.BRIGHT + "Nenhum dado encontrado sobre a Hash informada\n")
 
-        print(Fore.MAGENTA + Style.BRIGHT + '\n\n-=-=-=- X-FORCE -=-=-=-\n')
+        else:
+            malware = response['malware']
+            origins = malware['origins']
+            risco = malware['risk'].upper()
+            risco = risco == 'LOW' and Fore.GREEN + f"{risco}" or (risco == 'MEDIUM' and Fore.YELLOW + f"{risco}" or Fore.RED + f"{risco}")
+            tags = response['tags']
 
-        print(Fore.CYAN + Style.BRIGHT + '\n=== DETALHES ===\n')  
+            print(Fore.MAGENTA + Style.BRIGHT + '\n\n-=-=-=- X-FORCE -=-=-=-\n')
 
-        print(f'Tipo do arquivo: {malware["type"].upper()}')
-        print(f'Risco: {risco}')
+            print(Fore.CYAN + Style.BRIGHT + '\n=== DETALHES ===\n')  
 
-        if tags:
-            print("Tags:")
-            if len(tags) <= 10:
-                for i in tags:
-                    print(Fore.YELLOW + Style.BRIGHT + f"  -> {i['tag']}")
-            else:
-                i = 0
-                while i < 10:
-                    print(Fore.YELLOW + Style.BRIGHT + f"  -> {tags[i]['tag']}")
-                    i += 1
-                print(Fore.YELLOW + Style.BRIGHT + f'  -> Entre outras ({len(tags) - 10})')
+            print(f'Tipo do arquivo: {malware["type"].upper()}')
+            print(f'Risco: {risco}')
 
-        print(Fore.CYAN + Style.BRIGHT + '\n=== MALWARES ===\n')  
+            if tags:
+                print("Tags:")
+                if len(tags) <= 10:
+                    for i in tags:
+                        print(Fore.YELLOW + Style.BRIGHT + f"  -> {i['tag']}")
+                else:
+                    i = 0
+                    while i < 10:
+                        print(Fore.YELLOW + Style.BRIGHT + f"  -> {tags[i]['tag']}")
+                        i += 1
+                    print(Fore.YELLOW + Style.BRIGHT + f'  -> Entre outras ({len(tags) - 10})')
 
-        for i in origins:
-            print(f'Origem: {Fore.YELLOW + Style.BRIGHT + i}')
-            fonte = origins[i] 
-            if i == 'external':
+            print(Fore.CYAN + Style.BRIGHT + '\n=== MALWARES ===\n')  
 
-                print(f'Fonte da detecção: {fonte["source"]}')
-                print(f'Visto pela primeira vez: {fonte["firstSeen"]}')
-                print(f'Visto pela última vez: {fonte["lastSeen"]}')
-                print(f'Família do malware: {", ".join(fonte["family"])}')
-                print(f'Tipo do malware: {fonte["malwareType"]}')
-                print(f'Cobertura da comunidade: {fonte["detectionCoverage"]}%')
-                print(f'Plataforma: {fonte["platform"]}')
-                if "subPlatform" in fonte:
-                    print(f'Sub-plataforma: {fonte["subPlatform"]}')
+            for i in origins:
+                print(f'Origem: {Fore.YELLOW + Style.BRIGHT + i}')
+                fonte = origins[i] 
+                if i == 'external':
 
-            elif i != 'external' and not i == 'subjects':
-                
-                malware_info(origins[i])
+                    print(f'Fonte da detecção: {fonte["source"]}')
+                    print(f'Visto pela primeira vez: {fonte["firstSeen"]}')
+                    print(f'Visto pela última vez: {fonte["lastSeen"]}')
+                    print(f'Família do malware: {", ".join(fonte["family"])}')
+                    print(f'Tipo do malware: {fonte["malwareType"]}')
+                    print(f'Cobertura da comunidade: {fonte["detectionCoverage"]}%')
+                    print(f'Plataforma: {fonte["platform"]}')
+                    if "subPlatform" in fonte:
+                        print(f'Sub-plataforma: {fonte["subPlatform"]}')
+
+                elif i != 'external' and not i == 'subjects':
+                    
+                    malware_info(origins[i])
 
     except Exception as e:
         print(e.message, e.args)
--- APIs/ibm_xforce/ip_addresses.py
+++ APIs/ibm_xforce/ip_addresses.py
@@ -8,104 +8,117 @@ def xfr_get_ip(api, ip):
     response = requests.get(f"https://api.xforce.ibmcloud.com/api/ipr/{ip}", headers= {
         "Authorization": f"Basic {api}",
         "accept": "application/json"
-    }).json()
+    })
 
     response_malware = requests.get(f"https://api.xforce.ibmcloud.com/api/ipr/malware/{ip}", headers= {
         "Authorization": f"Basic {api}",
         "accept": "application/json"
-    }).json()
+    })
 
     try:
 
         print(Fore.MAGENTA + Style.BRIGHT + '\n\n-=-=-=- X-FORCE -=-=-=-\n')
-        print(Fore.CYAN + Style.BRIGHT + '\n=== DETALHES ===\n')
 
-        print(f'País: {response["geo"]["country"]}\n')
+        if response.status_code == 404:
+            print(Fore.RED + Style.BRIGHT + "Nenhum dado encontrado sobre informações gerais do IP informado\n")
+            
+        else:
+            response = response.json()
 
-        print('Sub-redes:')
-        subnets = response['subnets']
-        for i in subnets:
-            print(Fore.YELLOW + Style.BRIGHT + f'  -> {i["subnet"]}')
-            if "geo" in i:
-                print(f'     País: {i["geo"]["country"]}')
+            print(Fore.CYAN + Style.BRIGHT + '\n=== DETALHES ===\n')
 
-            for j in i["asns"].keys():
-                print(f'     ASN: {j}')
-                if "Company" in i["asns"][j]:
-                    print(f'     Companhia: {i["asns"][j]["Company"]}')
+            print(f'País: {response["geo"]["country"]}\n')
 
+            print('Sub-redes:')
+            subnets = response['subnets']
+            for i in subnets:
+                print(Fore.YELLOW + Style.BRIGHT + f'  -> {i["subnet"]}')
+                if "geo" in i:
+                    print(f'     País: {i["geo"]["country"]}')
+                if "asns" in i:
+                    for j in i["asns"].keys():
+                        print(f'     ASN: {j}')
+                        if "Company" in i["asns"][j]:
+                            print(f'     Companhia: {i["asns"][j]["Company"]}')
 
-            risco = i["score"]
-            risco = risco < 4 and Fore.GREEN + f"{risco}" or (risco < 7 and Fore.YELLOW + f"{risco}" or Fore.RED + f"{risco}")
-            print(Style.BRIGHT + f'     Risco: {risco}\n')
-        
-        risco = response["score"]
-        risco = risco < 4 and Fore.GREEN + f"{risco}" or (risco < 7 and Fore.YELLOW + f"{risco}" or Fore.RED + f"{risco}")
-        print(f'Risco do IP: {risco}')
-        print(f'Motivo: {response["reason"]}\n')
-
-        tags = response["tags"]
-        if tags:
-            print("Tags:")
-            if len(tags) <= 10:
-                for i in tags:
-                    print(Fore.YELLOW + Style.BRIGHT + f"  -> {i['tag']}")
-            else:
-                i = 0
-                while i < 10:
-                    print(Fore.YELLOW + Style.BRIGHT + f"  -> {tags[i]['tag']}")
-                    i += 1
-                print(Fore.YELLOW + Style.BRIGHT + f'  -> Entre outras ({len(tags) - 10})')
-
-        print(Fore.CYAN + Style.BRIGHT + '\n=== HISTÓRICO DE DETECÇÕES ===\n')
-
-        historico = response["history"]
-        historico.sort(key=lambda dict: dict["score"], reverse=True)
-
-        if len(historico) <= 10:
-            for i in historico:
-                dia, hora = i["created"].split('T')
-                dia = '-'.join(list(reversed(dia.split('-'))))
-                hora = hora.split('.')[0]
-                print(Fore.YELLOW + Style.BRIGHT + f'Data da detecção: {dia}, às {hora}')
-
-                if "malware_extended" in i:
-                    malware, *resto = i['malware_extended'].keys()
-                    print(Fore.RED + Style.BRIGHT + f'{malware}: {i["malware_extended"][malware]}')
 
                 risco = i["score"]
                 risco = risco < 4 and Fore.GREEN + f"{risco}" or (risco < 7 and Fore.YELLOW + f"{risco}" or Fore.RED + f"{risco}")
-                print(Style.BRIGHT + f'Risco: {risco}')
-                print(f'Motivo: {i["reason"]}\n')
+                print(Style.BRIGHT + f'     Risco: {risco}\n')
+            
+            risco = response["score"]
+            risco = risco < 4 and Fore.GREEN + f"{risco}" or (risco < 7 and Fore.YELLOW + f"{risco}" or Fore.RED + f"{risco}")
+            print(f'Risco do IP: {risco}')
+            print(f'Motivo: {response["reason"]}\n')
+
+            tags = response["tags"]
+            if tags:
+                print("Tags:")
+                if len(tags) <= 10:
+                    for i in tags:
+                        print(Fore.YELLOW + Style.BRIGHT + f"  -> {i['tag']}")
+                else:
+                    i = 0
+                    while i < 10:
+                        print(Fore.YELLOW + Style.BRIGHT + f"  -> {tags[i]['tag']}")
+                        i += 1
+                    print(Fore.YELLOW + Style.BRIGHT + f'  -> Entre outras ({len(tags) - 10})')
+
+
+            print(Fore.CYAN + Style.BRIGHT + '\n=== HISTÓRICO DE DETECÇÕES ===\n')
+
+            historico = response["history"]
+            historico.sort(key=lambda dict: dict["score"], reverse=True)
+
+            if len(historico) <= 10:
+                for i in historico:
+                    dia, hora = i["created"].split('T')
+                    dia = '-'.join(list(reversed(dia.split('-'))))
+                    hora = hora.split('.')[0]
+                    print(Fore.YELLOW + Style.BRIGHT + f'Data da detecção: {dia}, às {hora}')
+
+                    if "malware_extended" in i:
+                        malware, *resto = i['malware_extended'].keys()
+                        print(Fore.RED + Style.BRIGHT + f'{malware}: {i["malware_extended"][malware]}')
+
+                    risco = i["score"]
+                    risco = risco < 4 and Fore.GREEN + f"{risco}" or (risco < 7 and Fore.YELLOW + f"{risco}" or Fore.RED + f"{risco}")
+                    print(Style.BRIGHT + f'Risco: {risco}')
+                    print(f'Motivo: {i["reason"]}\n')
+
+            else:
+                i = 0
+                while i < 10:
+                    dia, hora = historico[i]["created"].split('T')
+                    dia = '-'.join(list(reversed(dia.split('-'))))
+                    hora = hora.split('.')[0]
+                    print(Fore.YELLOW + Style.BRIGHT + f'Data da detecção: {dia}, às {hora}')
+
+                    if "malware_extended" in historico[i]:
+                        malware, *resto = historico[i]['malware_extended'].keys()
+                        print(Fore.RED + Style.BRIGHT + f'{malware}: {historico[i]["malware_extended"][malware]}')
+
+                    risco = historico[i]["score"]
+                    risco = risco < 4 and Fore.GREEN + f"{risco}" or (risco < 7 and Fore.YELLOW + f"{risco}" or Fore.RED + f"{risco}")
+                    print(Style.BRIGHT + f'Risco: {risco}')
+                    print(f'Motivo: {historico[i]["reason"]}\n')
+                    
+                    i += 1
+
+                print(Fore.YELLOW + Style.BRIGHT + f'-> Entre outras ({len(historico) - 10})')
+
+
+        if response_malware.status_code == 404:
+            print(Fore.RED + Style.BRIGHT + "Nenhum dado encontrado sobre malwares do IP informado\n")
 
         else:
-            i = 0
-            while i < 10:
-                dia, hora = historico[i]["created"].split('T')
-                dia = '-'.join(list(reversed(dia.split('-'))))
-                hora = hora.split('.')[0]
-                print(Fore.YELLOW + Style.BRIGHT + f'Data da detecção: {dia}, às {hora}')
-
-                if "malware_extended" in historico[i]:
-                    malware, *resto = historico[i]['malware_extended'].keys()
-                    print(Fore.RED + Style.BRIGHT + f'{malware}: {historico[i]["malware_extended"][malware]}')
-
-                risco = historico[i]["score"]
-                risco = risco < 4 and Fore.GREEN + f"{risco}" or (risco < 7 and Fore.YELLOW + f"{risco}" or Fore.RED + f"{risco}")
-                print(Style.BRIGHT + f'Risco: {risco}')
-                print(f'Motivo: {historico[i]["reason"]}\n')
+            response_malware = response_malware.json()
+            malware = response_malware['malware']
+            if malware:
+                print(Fore.CYAN + Style.BRIGHT + '\n=== MALWARES ===\n')
                 
-                i += 1
-
-            print(Fore.YELLOW + Style.BRIGHT + f'-> Entre outras ({len(historico) - 10})')
-
-
-        malware = response_malware['malware']
-        if malware:
-            print(Fore.CYAN + Style.BRIGHT + '\n=== MALWARES ===\n')
-            
-            print('Quantidade total de detecções: ' + Fore.RED + Style.BRIGHT + f'{len(malware)}\n')
-            malware_info(malware)
+                print('Quantidade total de detecções: ' + Fore.RED + Style.BRIGHT + f'{len(malware)}\n')
+                malware_info(malware)
 
 
     except Exception as e:
